73d87751aba217075fd4d07e5e2afc64808c9404
Fedir Buzhylov <0961120962fb@gmail.com>
2024-10-10 10:41:08 +0300

    Update .net from 5 to 8. And seted up docker

---
+++ SagaGamesHub/SagaGamesHub.WebApi/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SagaGamesHub.WebApi.Controllers
+{
+    [Route("api/test")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public string Get() => "Test Controller works";
+    }
+}
